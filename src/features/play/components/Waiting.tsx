c7816fb39612216d94dc2ed1d4b853110c87f3fc
yuta925 <yutarou041@gmail.com>
2024-06-21 02:41:05 +0900

    待機中にローディングを表示

--- src/features/play/components/Waiting.tsx
+++ src/features/play/components/Waiting.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import styles from "./Waiting.module.css";
 
 export default function Waiting() {
-  return <div className="text-gray-500">他のプレイヤーが操作中</div>;
+  return (
+    <div>
+      <div className="text-gray-500">他のプレイヤーが操作中</div>
+      <div id="loading" className={styles.loading}>
+        <div className={styles.loadingCircle}></div>
+        <span className={styles.loadingTitle}>...loading</span>
+      </div>
+    </div>
+  );
 }
